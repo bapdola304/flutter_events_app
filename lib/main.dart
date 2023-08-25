7953f40f1afe63d245dc36b011b1fe710746b0d8
Hưng Ngô <bapdola304@gmail.com>
2023-08-25 15:03:30 +0700

    done plash screen

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,13 @@ import 'package:events_app/screens/profile.dart';
 import 'package:events_app/screens/search.dart';
 import 'package:events_app/screens/tickets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
